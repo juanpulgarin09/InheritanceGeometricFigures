d7885feceb41dbb339e7fd37772eecd84f64ba9c
juanpulgarin09 <juanpulgarin.09az@gmail.com>
2026-03-03 10:48:10 -0500

    Add class circle

---
+++ InheritanceGeometricFigures/GeometricFigures.Backend/Circle.cs
@@ -0,0 +1,27 @@
+﻿namespace GeometricFigures.Backend;
+
+public class Circle : GeometricFigure
+{
+
+    private double _r;
+
+    public Circle(string name, double r) : base(name)
+    {
+        R = r;
+    }
+
+    public double R
+    {
+        get => _r;
+        set => _r = value;
+    }
+
+    public override double GetArea() => Math.PI * _r * _r;
+    public override double GetPerimeter() => 2 * Math.PI * _r;
+
+    private double ValidateR(double value)
+    {
+        if (value <= 0) throw new ArgumentException("R must be greater than 0");
+        return value;
+    }
+}
---
+++ InheritanceGeometricFigures/GeometricFigures.Backend/CircleBase.cs
@@ -0,0 +1,8 @@
+﻿namespace GeometricFigures.Backend
+{
+    public class CircleBase
+    {
+
+        public override double GetArea() => Math.PI * _r * _r;
+    }
+}
