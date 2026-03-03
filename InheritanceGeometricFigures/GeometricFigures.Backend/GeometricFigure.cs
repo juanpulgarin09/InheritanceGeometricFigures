2a5b5320fa17965816b500e8aa350d2c1c2f7d66
juanpulgarin09 <juanpulgarin.09az@gmail.com>
2026-03-02 19:10:54 -0500

    Abstract class GeometricFigure

---
+++ InheritanceGeometricFigures/GeometricFigures.Backend/GeometricFigure.cs
@@ -0,0 +1,19 @@
+﻿namespace GeometricFigures.Backend;
+
+public abstract class GeometricFigure(string name)
+{
+
+    public string Name
+    {
+        get;
+        set;
+    } = name;
+
+    public abstract double GetArea();
+    public abstract double GetPerimeter();
+
+    public override string ToString()
+    {
+        return $"{Name,-15} => Area.....: {GetArea(),14:F5}   Perimiter: {GetPerimeter(),14:F5}";
+    }
+}
